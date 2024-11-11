7427b47f953e92f4a48b657e4f5d4fec4a389268
Nowaha <noahverkaik@gmail.com>
2024-11-11 17:44:53 +0100

    Guh

--- Godot/mods/TitlesPlus/main.gd
+++ Godot/mods/TitlesPlus/main.gd
@@ -4,27 +4,10 @@ const DEBUG = false
 const TITLE_CHANNEL = 452329
 const PREFIX = "[TitlesPlus] "
 
-signal _sendTitle(new_title)
-
-onready var title_api = get_node_or_null("/root/TitleAPI")
-
-var PlayerNode
-var STEAMID
-var TITLE_CHANNEL = 452329
-
-var prefix = "[TitlesPlus] "
-
-onready var steam_network = get_node_or_null("/root/Network")
-
-func get_player_node():
-	return get_tree().current_scene.get_node_or_null("Viewport/main/entities/player")
+onready var title_api = get_node("/root/TitleAPI")
 
 func sendTitle(custom_title):
-	emit_signal("_sendTitle")
 	update_title(custom_title)
-
-func update_title(title):
-	steam_network._send_P2P_Packet({"type": "update_title", "title": title}, "peers", 2, TITLE_CHANNEL)
 
 func update_title_packet(sender, packet):
 	var title = packet.get("title", null)
@@ -35,19 +18,9 @@ func update_title_packet(sender, packet):
 	if str(title).length() > 32:
 		print(PREFIX + "Illegal packet > 32 characters received")
 		return
+
 	title_api.register_title(sender, title)
 
-func _ready():
-	STEAMID = steam_network.STEAM_ID
-	
-	print(prefix + "Loaded! - Your SteamID: " + str(STEAMID))
-	
-	get_tree().connect("node_added", self, "_node_added")
-
-func _node_added(node):
-	if node.name == "player":
-		PlayerNode = node
-		
 func _inject(hud):
 	_print_debug("I have injected to the hud  - 1")
 	var main: Node = hud.get_child(0)
